5b5c139335b9ca1f90ac5c7959e8fb35b23e9469
AysunShirelizade1 <sirliyevaaysun93@gmail.com>
2026-05-10 19:35:11 +0400

    Appointment validators

--- Application/PetCareMini.Application/DTOs/Appointment/AppointmentStatusUpdateDto.cs
+++ Application/PetCareMini.Application/DTOs/Appointment/AppointmentStatusUpdateDto.cs
@@ -4,7 +4,6 @@ namespace PetCareMini.Application.DTOs.Appointment;
 
 public class AppointmentStatusUpdateDto
 {
-    // Added Range validation so invalid numbers are rejected
-    [Range(0, 3, ErrorMessage = "Status must be between 0 and 3.")]
+    
     public int Status { get; set; } // 0=Pending, 1=Approved, 2=Completed, 3=Canceled
 }
---
+++ Application/PetCareMini.Application/Validators/Appointment/AppointmentStatusUpdateDtoValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using PetCareMini.Application.DTOs.Appointment;
+
+namespace PetCareMini.Application.Validators.Appointment;
+
+public class AppointmentStatusUpdateDtoValidator : AbstractValidator<AppointmentStatusUpdateDto>
+{
+    public AppointmentStatusUpdateDtoValidator()
+    {
+        RuleFor(x => x.Status)
+            .InclusiveBetween(0, 3).WithMessage("Status must be between 0 and 3. (0=Pending, 1=Approved, 2=Completed, 3=Canceled)");
+    }
+}
