552ecfa501e69b88aa97b7227f495ac8d34790e5
AysunShirelizade1 <sirliyevaaysun93@gmail.com>
2026-05-10 19:24:31 +0400

    Auth Validators

---
+++ Application/PetCareMini.Application/Validators/Auth/RegisterDtoValidator.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+using PetCareMini.Application.DTOs.Auth;
+
+namespace PetCareMini.Application.Validators.Auth;
+
+public class RegisterDtoValidator : AbstractValidator<RegisterDto>
+{
+    public RegisterDtoValidator()
+    {
+        RuleFor(x => x.FullName)
+            .NotEmpty().WithMessage("Full name is required.")
+            .MinimumLength(2).WithMessage("Full name must be at least 2 characters.")
+            .MaximumLength(100).WithMessage("Full name must not exceed 100 characters.");
+
+        RuleFor(x => x.Email)
+            .NotEmpty().WithMessage("Email is required.")
+            .EmailAddress().WithMessage("Email format is invalid.")
+            .MaximumLength(100).WithMessage("Email must not exceed 100 characters.");
+
+        RuleFor(x => x.Password)
+            .NotEmpty().WithMessage("Password is required.")
+            .MinimumLength(6).WithMessage("Password must be at least 6 characters.")
+            .MaximumLength(50).WithMessage("Password must not exceed 50 characters.")
+            .Matches("[A-Z]").WithMessage("Password must contain at least one uppercase letter.")
+            .Matches("[a-z]").WithMessage("Password must contain at least one lowercase letter.")
+            .Matches("[0-9]").WithMessage("Password must contain at least one number.");
+    }
+}
--- Persistence/PetCareMini.Persistence/ServiceRegistration.cs
+++ Persistence/PetCareMini.Persistence/ServiceRegistration.cs
@@ -1,10 +1,13 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using FluentValidation;
+using FluentValidation.AspNetCore;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using PetCareMini.Application.Abstracts.Repositories;
+using PetCareMini.Application.Abstracts.Services;
+using PetCareMini.Application.Validators.Auth;
 using PetCareMini.Persistence.Contexts;
 using PetCareMini.Persistence.Repositories;
-using PetCareMini.Application.Abstracts.Services;
 using PetCareMini.Persistence.Services;
 namespace PetCareMini.Persistence;
 
@@ -16,8 +19,10 @@ public static class ServiceRegistration
     {
         services.AddDbContext<AppDbContext>(options =>
             options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+        // FluentValidation
+        services.AddFluentValidationAutoValidation();
+        services.AddValidatorsFromAssemblyContaining<LoginDtoValidator>();
         #region Repositories
-        services.AddScoped<IPetRepository, PetRepository>();
         services.AddScoped<IVeterinarianRepository, VeterinarianRepository>();
         services.AddScoped<IServiceRepository, ServiceRepository>();
         services.AddScoped<IProductCategoryRepository, ProductCategoryRepository>();
