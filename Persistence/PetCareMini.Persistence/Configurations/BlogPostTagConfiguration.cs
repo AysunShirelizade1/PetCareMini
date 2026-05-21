d6a8f07737f5d784668820a4133d301e41f2ed72
be302_first <sirliyevaaysun93@gmail.com>
2026-05-21 16:05:07 +0400

    feat: Blog Configurations/ migration

---
+++ Persistence/PetCareMini.Persistence/Configurations/BlogPostTagConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PetCareMini.Domain.Entities;
+
+namespace PetCareMini.Persistence.Configurations;
+
+public class BlogPostTagConfiguration : IEntityTypeConfiguration<BlogPostTag>
+{
+    public void Configure(EntityTypeBuilder<BlogPostTag> builder)
+    {
+        // Composite primary key
+        builder.HasKey(x => new { x.BlogPostId, x.TagId });
+
+        builder.HasOne(x => x.BlogPost)
+            .WithMany(x => x.BlogPostTags)
+            .HasForeignKey(x => x.BlogPostId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasOne(x => x.Tag)
+            .WithMany(x => x.BlogPostTags)
+            .HasForeignKey(x => x.TagId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
