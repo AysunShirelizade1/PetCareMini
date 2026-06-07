1498473e65b4756e1887ceb231b4c242bc616982
be302_first <sirliyevaaysun93@gmail.com>
2026-06-07 12:56:27 +0400

    feat: fix email and auth service

--- Infrastructure/PetCareMini.Infrastructure/InfrastructureServiceRegistration.cs
+++ Infrastructure/PetCareMini.Infrastructure/InfrastructureServiceRegistration.cs
@@ -1,8 +1,8 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using PetCareMini.Application.Abstracts.Services;
+using PetCareMini.Application.Shared.Settings;
 using PetCareMini.Infrastructure.Services;
-using PetCareMini.Infrastructure.Settings;
 
 namespace PetCareMini.Infrastructure;
 
@@ -13,7 +13,11 @@ public static class InfrastructureServiceRegistration
         IConfiguration configuration)
     {
         services.Configure<EmailSettings>(configuration.GetSection("EmailSettings"));
+        services.Configure<CloudinarySettings>(configuration.GetSection("CloudinarySettings"));
+
         services.AddScoped<IEmailService, EmailService>();
+        services.AddScoped<ICloudinaryService, CloudinaryService>();
+
         return services;
     }
 }
