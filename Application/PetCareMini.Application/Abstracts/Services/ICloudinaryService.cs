fc152a216ab989fd8f75696c9528cd68f586995b
be302_first <sirliyevaaysun93@gmail.com>
2026-06-07 12:21:55 +0400

    feat: Add Cloudinary email services

---
+++ Application/PetCareMini.Application/Abstracts/Services/ICloudinaryService.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace PetCareMini.Application.Abstracts.Services;
+
+public interface ICloudinaryService
+{
+    Task<(string ImageUrl, string PublicId)> UploadImageAsync(IFormFile file, string folderName);
+    Task<bool> DeleteImageAsync(string publicId);
+}
