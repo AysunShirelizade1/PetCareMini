e39f5b2bf4608a9c5952686de041a83d300d1a1c
be302_first <sirliyevaaysun93@gmail.com>
2026-06-07 13:25:14 +0400

    feat:Add validators to user

---
+++ Application/PetCareMini.Application/Validators/User/ChangePasswordDtoValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using PetCareMini.Application.DTOs.User;
+
+namespace PetCareMini.Application.Validators.User;
+
+public class ChangePasswordDtoValidator : AbstractValidator<ChangePasswordDto>
+{
+    public ChangePasswordDtoValidator()
+    {
+        RuleFor(x => x.CurrentPassword)
+            .NotEmpty().WithMessage("Cari şifrə boş ola bilməz.");
+
+        RuleFor(x => x.NewPassword)
+            .NotEmpty().WithMessage("Yeni şifrə boş ola bilməz.")
+            .MinimumLength(6).WithMessage("Yeni şifrə minimum 6 simvol olmalıdır.")
+            .MaximumLength(50).WithMessage("Yeni şifrə 50 simvoldan çox ola bilməz.");
+    }
+}
